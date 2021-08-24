c645aab3c6f63b75000cd65f8bcd529471e51bf0
armstrongsouljah <armstrongsouljah@gmail.com>
2021-08-24 08:47:12 +0300

    Respond to button events and add ui elements

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView2: UIImageView! // ctrl click and drag to view controller to create reference.
+    // UI references are created with IBOutlets
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")  // set the image attribute with code
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func diceRollButton(_ sender: UIButton) {
+        // respond to the button events
+        // events handled here aren't dependant on viewLoad event.
+        print("You tapped me!")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
